d8193003340b1417a47cbb840a29f9bb4ae647ef
csoumya14 <soumyachalakkal@gmail.com>
2015-09-05 17:41:40 +0200

    a small change made

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Soumya on 24/08/15.
+//  Copyright © 2015 Soumya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello!\n";
+    return 0;
+}
